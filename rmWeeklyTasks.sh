13a5af7673221e80453c9b8affcf95ab79186b4b
gapolinario <gabrielbritoapolinario@gmail.com>
2017-11-10 14:01:36 -0200

    More to the rmWeeklyTasks function

--- rmWeeklyTasks.sh
+++ rmWeeklyTasks.sh
@@ -18,3 +18,9 @@ enddate="$2"
 # gawk '!/\|\*/' apts
 
 gawk -v sd=$1 -v ed=$2 '/\|\*/ { if ( $1 < sd || $1 > ed ) print $0  }' $input
+gawk '!/\|\*/' $input
+
+# this works, but it has two problems
+# 1. does not order the tasks, but Calcurse does this automatically
+# 2. i have to make a copy of the input file, or all the non auto-tasks will be lost
+# in summary, it is not ready yet
