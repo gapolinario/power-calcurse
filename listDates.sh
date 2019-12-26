e953fb2d39bbae4d23d05c515b463d077389f663
gapolinario <gabrielbritoapolinario@gmail.com>
2019-12-26 10:35:33 -0300

    Simple program to list future days

---
+++ listDates.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# call with:
+# bash listDates.sh > path/to/file
+
+for i in `seq 1 21`
+do
+	future="+""$i"" day"
+	fullDate="$(date +'%a, %d %b' --date="$future")"
+
+        echo $fullDate
+	
+done
+
+
