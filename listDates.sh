8242be351494316ac13aa06df522cca603041d26
gapolinario <gabrielbritoapolinario@gmail.com>
2020-07-15 09:40:35 -0300

    ...

--- listDates.sh
+++ listDates.sh
@@ -2,10 +2,10 @@
 # call with:
 # bash listDates.sh > path/to/file
 
-for i in `seq 1 21`
+for i in `seq 1 150`
 do
 	future="+""$i"" day"
-	fullDate="$(date +'%a, %d %b' --date="$future")"
+	fullDate="$(date +'# %a, %d %b' --date="$future")"
 
         echo $fullDate
 	
